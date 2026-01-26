0822a798095f16ab6faebd6c7a8dc74646df9838
Your0ff <mr.ilya.yurov.2001@list.ru>
2026-01-26 22:27:22 +0300

    Students page Update

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Инициализация Bootstrap компонентов и другая логика
     console.log('Приложение загружено');
+    
+    // Фильтрация по группам
+    const groupSelect = document.getElementById('group');
+    if (groupSelect) {
+        groupSelect.addEventListener('change', function() {
+            const selectedGroup = this.value;
+            const url = new URL(window.location.href);
+            if (selectedGroup === 'Все группы') {
+                url.searchParams.delete('group');
+            } else {
+                url.searchParams.set('group', selectedGroup);
+            }
+            window.location.href = url.toString();
+        });
+    }
 });
---
+++ public/js/student.js
@@ -0,0 +1,6 @@
+
+
+
+export default function student() {
+    console.log('student');
+}
